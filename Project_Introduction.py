29dd697296fc977f53dd158c468fecc45e0ea1a0
RubyRose <rubyrosetat@gmail.com>
2022-01-03 14:52:17 +0800

    Update 1.3

--- Project_Introduction.py
+++ Project_Introduction.py
@@ -3,15 +3,15 @@ from PIL import Image
 
 def introduction():
     st.sidebar.markdown('---')
-    st.sidebar.image(Image.open('data/bilibili.jpeg'))
+    st.sidebar.image(Image.open('data/bilibili.jpg'))
 
     st.markdown("""
     # BiliBili视频数据分析预测
-    #### [项目地址](https://github.com/RubyRose-TAT/bilibili_Data_analysis-Clout_prediction)
+    #### 项目地址: [github](https://github.com/RubyRose-TAT/bilibili_Data_analysis-Clout_prediction)
     """)
 
     st.markdown("""
-    #### 项目成员 黄胜鸿 钟珺涛
+    #### 在线演示: [share.streamlit.io](https://share.streamlit.io/rubyrose-tat/bilibili_data_analysis-clout_prediction/main/main.py)
     """)
 
     st.markdown('---')
@@ -19,9 +19,9 @@ def introduction():
     st.markdown("""
                 # 1. 项目背景
 
-                随着互联网的发展，越来越多的人开始从事自媒体的工作。有些创作者靠着一些火热的视频收获了不少。但还有一些创作者的视频热度一直比较低。分析出一些热门视频的“秘密”必将对新人创作者提供一些帮助。
+                随着互联网的发展，越来越多的人开始从事自媒体的工作。有些创作者靠着一些火热的视频收获了不少，但还有一些创作者的视频热度一直比较低。分析出一些热门视频的“秘密”必将对新人创作者提供一些帮助。
 
-                本项目一开始将分析B站视频的一些热门视频的播放量与各大因素的关系，最后通过一些集成学习算法对播放量进行预测
+                *本项目将分析B站视频的一些热门视频的播放量与各大因素的关系，随后使用集成学习模型对播放量进行预测*
     """)
     with st.expander(""):
         st.image(Image.open('data/backgrand.png'), caption='')
@@ -30,31 +30,36 @@ def introduction():
 
                 # 2. 项目分工
 
-                ## 主要工作
+                #### 黄胜鸿：
+                1. 编写爬虫程序，数据爬取
+                2. 进行数据清洗和特征工程
+                3. Adaboost模型搭建与调参
+                4. 利用pyechart对数据集可视化分析
 
-                |  成员  | 主要工作                                                     |
-                | :----: | :----------------------------------------------------------- |
-                | 黄胜鸿 | 1.编写爬虫程序，数据爬取  2.责数据的清洗，特征处理  3.Adaboost模型搭建与调参  4.利用pyechart可视化分析 |
-                | 钟珺涛 | 1.探索性数据分析 2.集成学习模型搭建（Random Forest, GBDT, XGBoost, LIghtGBM） 3. 模型评估，参数调整 4.web页面制作 |
+                #### 钟珺涛：
+                1. 探索性数据分析
+                2. 集成学习模型搭建(Random Forest, GBDT, XGBoost, LIghtGBM)
+                3. 模型评估
+                4. web应用制作
 
                 # 3. 相关技术
 
-                ## 3.1数据获取
+                ### 3.1数据获取
 
                 Xpath定位与请求数据接口获取数据
 
-                ## 3.2可视化
+                ### 3.2可视化
 
-                #### 3.2.1数据可视化
+                ###### 3.2.1数据可视化
 
                 可视化的过程中，我们使用了python强大的可视化神器——pyechart, 使用它可以绘制出一些好看的图，让数据的呈现更加直观
 
-                #### 3.2.2可视化平台搭建
+                ###### 3.2.2可视化平台搭建
 
                 基于streamlit搭建web应用。Streamlit是一个开源的，可以用于快速搭建Web应用的Python库。
                 Streamlit官方的定位是服务于机器学习和数据科学的Web应用框架。它能够快速的帮助我们创建定制化的web应用，而且还非常便于和他人分享。只需要将项目保存到github中，项目下需要有requirements.txt文件，就能在https://share.streamlit.io/  上部署。
 
-                #### 3.3运用模型
+                ### 3.3运用模型
 
                 ###### 3.3.1 随机森林
 
@@ -62,35 +67,34 @@ def introduction():
 
                 ###### 3.3.2 GBDT
 
-                是一种基于boosting集成学习思想的加法模型，训练时采用前向分布算法进行贪婪的学习，每次迭代都学习一棵CART树来拟合之前 t-1 棵树的预测结果与训练样本真实值的残差。
+                由多棵决策树组成,所有树的结论累加起来做最终答案
 
                 ###### 3.3.3 XGBoost
 
-                极致梯度提升，是一种基于GBDT的算法或者说工程实现。
+                XGBoost高效地实现了GBDT算法并进行了算法和工程上的许多改进
 
                 ###### 3.3.4 LightGBM
 
-                LightGBM依然是在GBDT算法框架下的一种改进实现，是一种基于决策树算法的快速、分布式、高性能的GBDT框架，主要说解决的痛点是面对高维度大数据时提高GBDT框架算法的效率和可扩展性
+                LightGBM是在GBDT算法框架下的一种改进实现，是一种基于决策树算法的快速、分布式、高性能的GBDT框架
 
                 # 4.结论
 
-                ## 对热度的影响因素
+                ### 对热度的影响因素
     """)
 
-    with st.expander(""):
-        st.image(Image.open('data/XGBoost_shap.png'), caption='')
+    st.image(Image.open('data/XGBoost_shap.png'), caption='')
 
     st.markdown("""
 
-                可以看出：对播放量的预测中，重要度  点赞>弹幕>收藏>分享>发布时间间隔>硬币
+                可以看出：对一个视频播放量影响程度：  点赞>分享>收藏>发布时间>硬币
 
-                ## 给新人创作者的建议
+                ### 给新人创作者的建议
 
-                针对标题：在标题里可以使用一些表示疑问的词儿，还可以出现与我们标题高频词汇
+                - 针对标题：在标题里可以使用一些表示疑问的词儿，还可以出现与我们标题高频词汇
 
-                针对视频时长：控制6分钟以内
+                - 针对视频时长：控制6分钟以内
 
-                针对投稿分区：尽量投稿播放量比较高的分区，比如说
+                - 针对投稿分区：尽量投稿播放量比较高的分区，比如说
 
-                针对内容：重视内容质量提升
+                - 针对内容：重视内容质量提升
     """)
--- README.md
+++ README.md
@@ -1,8 +1,49 @@
-# bilibili_Data_analysis-Clout_prediction
-## b站视频热度预测与分析
-### 项目成员: 黄胜鸿 钟珺涛
-基于**streamlit**搭建的B站视频数据分析web应用
-获取B站热门视频数据集，数据可视化分析，对视频热度回归预测
+# b站视频热度预测与分析
 
-演示[demo](https://share.streamlit.io/rubyrose-tat/bilibili_data_analysis-clout_prediction/main/main.py)
+### 项目成员: 钟珺涛 黄胜鸿
+基于**streamlit**搭建的B站视频数据分析与播放量预测web应用
+获取B站热门视频数据集，数据可视化分析，对视频播放量回归预测
 
+在线演示: [share.streamlit.io](https://share.streamlit.io/rubyrose-tat/bilibili_data_analysis-clout_prediction/main/main.py)
+项目地址: [github](https://github.com/RubyRose-TAT/bilibili_Data_analysis-Clout_prediction)
+
+---
+
+### 运行项目
+
+##### 下载代码:
+
+* git clone
+
+```bash
+git clone git@github.com/RubyRose-TAT/bilibili_Data_analysis-Clout_prediction.git
+```
+
+* Download ZIP
+
+```bash
+https://github.com/RubyRose-TAT/bilibili_Data_analysis-Clout_prediction
+```
+
+##### 安装依赖:
+
+```bash
+pip install -r requirements.txt
+```
+
+##### 启动项目:
+
+```bash
+streamlit run main.py
+```
+
+---
+
+### Jupyter Notebook
+
+- [数据预处理及探索性数据分析](https://github.com/RubyRose-TAT/bilibili_Data_analysis-Clout_prediction/blob/main/bilibili/0.bilibili_%E6%95%B0%E6%8D%AE%E9%A2%84%E5%A4%84%E7%90%86%E5%8F%8A%E6%8E%A2%E7%B4%A2%E6%80%A7%E6%95%B0%E6%8D%AE%E5%88%86%E6%9E%90.ipynb)
+- [视频数据可视化分析](https://github.com/RubyRose-TAT/bilibili_Data_analysis-Clout_prediction/blob/main/bilibili/1.bilibili_%E8%A7%86%E9%A2%91%E6%95%B0%E6%8D%AE%E5%8F%AF%E8%A7%86%E5%8C%96%E5%88%86%E6%9E%90.ipynb)
+- [Random Forest](https://github.com/RubyRose-TAT/bilibili_Data_analysis-Clout_prediction/blob/main/bilibili/2.bilibili_Random%20Forest.ipynb)
+- [GBDT](https://github.com/RubyRose-TAT/bilibili_Data_analysis-Clout_prediction/blob/main/bilibili/3.bilibili_GBDT.ipynb)
+- [XGBoost](https://github.com/RubyRose-TAT/bilibili_Data_analysis-Clout_prediction/blob/main/bilibili/4.bilibili_XGBoost.ipynb)
+- [LIghtGBM](https://github.com/RubyRose-TAT/bilibili_Data_analysis-Clout_prediction/blob/main/bilibili/5.bilibili_LIghtGBM.ipynb)
